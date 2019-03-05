0debb0d9b6330220a78804b07bb3367407aac9b1
simonwagner <swagner648@gmail.com>
2019-03-04 21:43:42 -0600

    Homework 5 complete

--- homework_5/problem_11.py
+++ homework_5/problem_11.py
@@ -4,24 +4,8 @@ import math
 import warnings
 
 
-def newton_compute(x, i, j):
-    p = 1
-    for k in range(0, j):
-        p *= (x[i] - x[k])
-    return p
-
-
 def newton_form_coef(x, y):
-    n = len(x)
-    coef = [[0 for x in range(n)] for y in range(n)]
-    p = 1
-    for i in range(0, n):
-        for j in range(0, n):
-            if j == 0:
-                coef[i][j] = 1
-            elif j <= i:
-                coef[i][j] = newton_compute(x, i, j)
-    return np.linalg.solve(coef, y)
+    return np.linalg.solve(np.vander(x), y)
 
 
 def newton_div_coef(x, y):
@@ -30,29 +14,28 @@ def newton_div_coef(x, y):
     for j in range(1, n):
         for i in range(n - 1, j - 1, -1):
             coef[i] = (coef[i] - coef[i - 1]) / (x[i] - x[i - j])
-    print(coef)
     return coef
 
 
 def lagrange_coef(x, y):
     n = len(x)
-    l = [1] * n
-    coef = [0] * n
+    Lj = np.array([np.array([0.0 for i in range(n)]) for i in range(n)])
+
     for i in range(0, n):
-        for j in range(0, n):
-            if i != j:
-                l[i] *= 1 / (x[i] - x[j])
-                coef[i] = y[i] * l[i]
-    return coef
+        m = 1.0
+        c = 1.0
+        for k in range(0, n):
+            if k != i and x[k] != x[i]:
+                c = np.convolve(c, [1, -x[k]])
+                m = m * (x[i] - x[k]) ** -1
+        Lj[i, :] = m * c
+    return np.matmul(y, Lj)
 
 
 def horner_eval(coef, x):
-    coef = coef[::-1]
-    p = coef[-1]
-    i = len(coef) - 2
-    while i >= 0:
+    p = coef[0]
+    for i in range(1, len(coef)):
         p = p * x + coef[i]
-        i -= 1
     return p
 
 
@@ -61,8 +44,7 @@ def res_eval(p, x, y, k):
     for i, sample_point in enumerate(x):
         tmp = horner_eval(p, sample_point) - y[i]
         if k == 0:
-            prev = abs(tmp)  # Swap for the max
-            if prev > norm:
+            if abs(tmp) > norm:
                 norm = tmp
         elif k == 1:
             norm += abs(tmp)
@@ -72,27 +54,41 @@ def res_eval(p, x, y, k):
 
 
 def run(f, lb, ub, letter):
-    x_points = [i for i in np.arange(lb, ub, (ub - lb) / 201)]
+    x_points = [i for i in np.linspace(lb, ub, 201)]
     y_points = [f(i) for i in x_points]
 
     nfc = newton_form_coef(x_points, y_points)
     ndc = newton_div_coef(x_points, y_points)
     lc = lagrange_coef(x_points, y_points)
 
-    polyfit = poly.polyfit(x_points, y_points, 200)
+    polyfit = poly.polyfit(x_points, y_points, 200)[::-1]
 
-    x_points = [i for i in np.arange(lb, ub, (ub - lb) / 1001)]
+    x_points = [i for i in np.linspace(lb, ub, 1001)]
     y_points = [f(i) for i in x_points]
     print(f"-----------------------------------\nResults for ({letter}):")
-    print("nfc:\t", res_eval(nfc, x_points, y_points, 0))
-    print("ndc:\t", res_eval(ndc, x_points, y_points, 0))
-    print("lc:\t\t", res_eval(lc, x_points, y_points, 0))
-    print("polyfit:", res_eval(polyfit, x_points, y_points, 0))
+    print("L  Algthm \t Residual")
+    print("0  nfc:\t\t", res_eval(nfc, x_points, y_points, 0))
+    # print("0  ndc:\t\t", res_eval(ndc, x_points, y_points, 0))
+    print("0  lc:\t\t", res_eval(lc, x_points, y_points, 0))
+    print("0  polyfit:\t", res_eval(polyfit, x_points, y_points, 0))
+    print("1  nfc:\t\t", res_eval(nfc, x_points, y_points, 1))
+    # print("1  ndc:\t\t", res_eval(ndc, x_points, y_points, 1))
+    print("1  lc:\t\t", res_eval(lc, x_points, y_points, 1))
+    print("1  polyfit:\t", res_eval(polyfit, x_points, y_points, 1))
+    print("2  nfc:\t\t", res_eval(nfc, x_points, y_points, 2))
+    # print("2  ndc:\t\t", res_eval(ndc, x_points, y_points, 2))
+    print("2  lc:\t\t", res_eval(lc, x_points, y_points, 2))
+    print("2  polyfit:\t", res_eval(polyfit, x_points, y_points, 2))
 
 
-# run(math.sin, 0, math.pi, 'a')
-# run(math.cos, 0, math.pi, 'b')
-# run(math.log, 0.5, 2, 'c')
-# run(math.exp, 0, 2, 'd')
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    run(math.sin, 0, 2 * math.pi, 'a')
+    run(math.cos, 0, 2 * math.pi, 'b')
+    run(math.log, 0.5, 2, 'c')
+    run(math.exp, 0, 2, 'd')
 
-newton_div_coef([0, 1, -1], [1, 3, 1])
+# print(newton_form_coef([-4, -2, 0], [9, 3, 5]))
+# print(newton_div_coef([-4, -2, 0], [9, 3, 5]))
+# print(lagrange_coef([-4, -2, 0], [9, 3, 5]))
+# print(poly.polyfit([-4, -2, 0], [9, 3, 5], 2)[::-1])
--- homework_5/problem_12.py
+++ homework_5/problem_12.py
@@ -8,20 +8,20 @@ def f(x):
     return 1 / (1 + 25 * x ** 2)
 
 
-a_x = np.arange(-1, 1, 2 / 100)
+a_x = np.linspace(-1, 1, 100)
 a_y = [f(i) for i in a_x]
+p = np.poly1d(poly.polyfit(a_x, a_y, 10)[::-1])
+polyfit = [p(i) for i in a_x]
 a_cx = [math.cos(math.pi * i / 10) for i in range(11)]
 a_cy = [f(i) for i in a_cx]
-p = np.poly1d(poly.polyfit(a_x, a_y, 10))
-polyfit = [p(i) for i in a_x]
-cp = np.poly1d(poly.polyfit(a_cx, a_cy, 10))
-cpolyfit = [cp(i) for i in a_cx]
+cp = np.poly1d(poly.polyfit(a_cx, a_cy, 10)[::-1])
+cpolyfit = cp(a_x)
 
 plt.figure(figsize=(15, 15))
 plt.plot(a_x, a_y, 'b--', label="Standard Points")
 plt.plot(a_cx, a_cy, 'r^', label="Chebyshev Points")
 plt.plot(a_x, polyfit, label="Standard Polyfit")
-plt.plot(a_cx, cpolyfit, label="Chebyshev Polyfit")
+plt.plot(a_x, cpolyfit, label="Chebyshev Polyfit")
 plt.title("Polyfit Sample Comparisons")
 plt.legend()
 plt.show()
